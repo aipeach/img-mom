aa47ed6b64099a09a3857462dd69d62a6b1cffca
aipeach <44671411+aipeach@users.noreply.github.com>
2024-08-25 01:14:08 +0800

    update

--- src/oss/cloudflareR2.ts
+++ src/oss/cloudflareR2.ts
@@ -2,19 +2,32 @@ import { genDateDirName } from "../utils";
 import { OSSProvider } from "./interface";
 
 class CloudflareR2 implements OSSProvider {
-	async uploadImage(data: any, fileName: string, fileType: string) {
-		const filePath = `${genDateDirName()}/${fileName}.${fileType}`;
-		await self.R2_IMG_MOM.put(filePath, data);
-		return filePath;
-	}
+    async uploadImage(data: any, fileName: string, fileType: string) {
+        const filePath = `${genDateDirName()}/${fileName}.${fileType}`;
+        await self.R2_IMG_MOM.put(filePath, data);
+        return filePath;
+    }
 
-	getURL(filePath: string) {
-		if (self.R2_CUSTOM_DOMAIN) {
-			return `https://${self.R2_CUSTOM_DOMAIN}/${filePath}`;
-		}
+    async uploadSticker(data: any, fileName: string, fileType: string) {
+        const filePath = `stickers/${genDateDirName()}/${fileName}.${fileType}`;
+        await self.R2_IMG_MOM.put(filePath, data);
+        return filePath;
+    }
 
-		return filePath;
-	}
+    async uploadFile(data: any, fileName: string, fileType: string) {
+        const filePath = `files/${genDateDirName()}/${fileName}.${fileType}`;
+        await self.R2_IMG_MOM.put(filePath, data);
+        return filePath;
+    }
+
+    getURL(filePath: string) {
+        if (self.R2_CUSTOM_DOMAIN) {
+            return `https://${self.R2_CUSTOM_DOMAIN}/${filePath}`;
+        }
+
+        return filePath;
+    }
 }
 
 export default CloudflareR2;
+
